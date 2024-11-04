3b1b3f40ea8f058e02a71e25578696802c7c452b
Your Name <you@example.com>
2024-11-04 22:04:28 +0600

    complate

--- src/Components/Page/ListedBook/ListedBook.jsx
+++ src/Components/Page/ListedBook/ListedBook.jsx
@@ -6,21 +6,29 @@ import WishList from "../WishList/WishList";
 
 const ListedBooks = () => {
     const [read, setRead] = useState([]);
+
     const [wish, setWish] = useState([]);
 
 
 
-    useEffect(() => {
-        const showWishList = JSON.parse(localStorage.getItem("wish")) || [];
-        setWish(showWishList);
-    }, []);
+
 
     useEffect(() => {
         const showDataRead = JSON.parse(localStorage.getItem("books")) || [];
         setRead(showDataRead);
     }, []);
 
+    useEffect(() => {
+        const showWishList = JSON.parse(localStorage.getItem("wish")) || [];
+        setWish(showWishList);
+    }, []);
 
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
 
     return (
         <div className="md:mt-[32px] mt-4 xl:mx-0 mx-2">
@@ -28,6 +36,39 @@ const ListedBooks = () => {
                 Books
             </h4>
 
+            <div className="relative inline-block text-left mt-2 mb-24 ">
+                <button
+                    onClick={toggleDropdown}
+                    className="bg-green-500 text-white font-semibold px-4 py-2 rounded-lg focus:outline-none w-44"
+                >
+                    Sort By <span className="ml-1">▼</span>
+                </button>
+
+                {isOpen && (
+                    <div className="absolute mt-2  bg-white rounded-lg shadow-lg z-10">
+                        <ul className="py-2">
+                            <li
+                                className="px-4 py-2 hover:bg-gray-200 cursor-pointer"
+                                onClick={() => console.log("Sort by Rating")}
+                            >
+                                Rating
+                            </li>
+                            <li
+                                className="px-4 py-2 hover:bg-gray-200 cursor-pointer"
+                                onClick={() => console.log("Sort by Number of pages")}
+                            >
+                                Number of pages
+                            </li>
+                            <li
+                                className="px-4 py-2 hover:bg-gray-200 cursor-pointer"
+                                onClick={() => console.log("Sort by Publisher year")}
+                            >
+                                Publisher year
+                            </li>
+                        </ul>
+                    </div>
+                )}
+            </div>
 
             <Tabs className="md:mt-14 mt-7">
                 <TabList>
--- src/Utility/LocalStorage.js
+++ src/Utility/LocalStorage.js
@@ -2,32 +2,32 @@ import { toast } from "react-toastify"
 
 
 const getLocalStorage = (details) => {
-    const storageData = JSON.parse(localStorage.getItem('books')) || []
+    const storageData = JSON.parse(localStorage.getItem('books')) || [];
     // console.log(storageData);
-    const savedData = storageData.find(item => item.bookId === details.bookId)
+    const savedData = storageData.find(item => item.bookId === details.bookId);
     if (savedData) {
-        toast.error('Already exists')
+        toast.error('Already exists');
     } else {
         storageData.push(details)
-        localStorage.setItem('books', JSON.stringify(storageData))
-        toast.success('Successfully Added')
+        localStorage.setItem('books', JSON.stringify(storageData));
+        toast.success('Successfully Added');
     }
 
 }
 
 const setLocalStorage = (details) => {
 
-    const storageData2 = JSON.parse(localStorage.getItem('wish')) || []
+    const storageData2 = JSON.parse(localStorage.getItem('wish')) || [];
     // console.log(storageData2);
 
-    const savedData2 = storageData2.find(item => item.bookId === details.bookId)
+    const savedData2 = storageData2.find(item => item.bookId === details.bookId);
     if (savedData2) {
-        toast.error('Already exists')
+        toast.error('Already exists');
     } else {
         storageData2.push(details)
-        localStorage.setItem('wish', JSON.stringify(storageData2))
-        toast.success('Successfully Added')
-    }
+        localStorage.setItem('wish', JSON.stringify(storageData2));
+        toast.success('Successfully Added');
+    };
 
 }
-export { getLocalStorage, setLocalStorage }
+export { getLocalStorage, setLocalStorage };
